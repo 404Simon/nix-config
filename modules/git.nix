2c68bcfc0b8037754615a73937a1b8db75a6df68
404Simon <s.wi@mail.de>
2026-04-19 20:36:18 +0200

    add gh dash

--- modules/git.nix
+++ modules/git.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
 
 {
   programs.git = {
@@ -32,4 +37,29 @@
   };
   programs.lazygit.enable = true;
   programs.gh.enable = true;
+  programs.gh.extensions = [
+    pkgs-unstable.gh-dash
+  ];
+  programs.gh-dash.enable = true;
+  programs.gh-dash.package = pkgs-unstable.gh-dash;
+  programs.gh-dash.settings = {
+    prSections = [
+      {
+        title = "My Repos";
+        filters = "is:open user:@me";
+      }
+      {
+        title = "My Pull Requests";
+        filters = "is:open author:@me";
+      }
+      {
+        title = "Needs My Review";
+        filters = "is:open review-requested:@me";
+      }
+      {
+        title = "Involved";
+        filters = "is:open involves:@me -author:@me";
+      }
+    ];
+  };
 }
