cd3affd0da08701d1a1931df0578d352b5c0afd7
404Simon <s.wi@mail.de>
2026-08-09 12:18:32 +0200

    more syncthing

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -6,6 +6,7 @@
 
     settings = {
       devices = {
+        pc.id = "SK3HCCN-ZXSDV3G-IPSDPSR-UP6PBVC-RZT4FMP-YNJZJEF-UDEZZJ4-A3ARHQ6";
         xmg.id = "V2MBCYZ-6HXPK3L-CELM2CO-CWLN4BQ-OZEPPF3-OUZYYHW-IC4SSBS-BXGOYQZ";
         dell.id = "3TPWPUA-QZHBKGH-VBB3KPR-AJOPAWO-LGXVKVX-P3YSWKI-XKNREPK-7VTCRQJ";
       };
@@ -13,9 +14,23 @@
       folders = {
         Music = {
           path = "/home/simon/Music";
-          type = "sendonly";
           devices = [
+            "pc"
             "xmg"
+            "dell"
+          ];
+        };
+        Documents = {
+          path = "/home/simon/Documents";
+          devices = [
+            "pc"
+            "dell"
+          ];
+        };
+        Downloads = {
+          path = "/home/simon/Downloads";
+          devices = [
+            "pc"
             "dell"
           ];
         };
